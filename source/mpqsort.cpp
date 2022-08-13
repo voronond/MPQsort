92a6cc2d40ae2e86f89f4ee031b47a367abd2c53
Ondrej Voronecky <ondrej.voronecky@icewarp.com>
2022-08-13 14:00:57 +0200

    FIX: Formatting

--- source/mpqsort.cpp
+++ source/mpqsort.cpp
@@ -1,20 +1,20 @@
-#include <mpqsort.h>
 #include <fmt/format.h>
+#include <mpqsort.h>
 
 using namespace mpqsort;
 
-    Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
+Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
 
-    std::string Greeter::greet(LanguageCode lang) const {
-        switch (lang) {
-            default:
-            case LanguageCode::EN:
-                return fmt::format("Hello, {}!", name);
-            case LanguageCode::DE:
-                return fmt::format("Hallo {}!", name);
-            case LanguageCode::ES:
-                return fmt::format("¡Hola {}!", name);
-            case LanguageCode::FR:
-                return fmt::format("Bonjour {}!", name);
-        }
+std::string Greeter::greet(LanguageCode lang) const {
+    switch (lang) {
+        default:
+        case LanguageCode::EN:
+            return fmt::format("Hello, {}!", name);
+        case LanguageCode::DE:
+            return fmt::format("Hallo {}!", name);
+        case LanguageCode::ES:
+            return fmt::format("¡Hola {}!", name);
+        case LanguageCode::FR:
+            return fmt::format("Bonjour {}!", name);
     }
+}
