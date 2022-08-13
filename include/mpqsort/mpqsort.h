e16431756e21148f2e1442b40e782d4e229d2ad1
Ondrej Voronecky <ondrej.voronecky@icewarp.com>
2022-08-13 12:01:27 +0200

    Changed indentation from 2 to 4 spaces

--- include/mpqsort/mpqsort.h
+++ include/mpqsort/mpqsort.h
@@ -1,48 +1,49 @@
 #pragma once
 
-#include <string>
 #include <fmt/format.h>
+
+#include <string>
 
 namespace mpqsort {
 
-  /**  Language codes to be used with the Greeter class */
-  enum class LanguageCode { EN, DE, ES, FR };
-
-  /**
-   * @brief A class for saying hello in multiple languages
-   */
-  class Greeter {
-    std::string name;
-
-  public:
-    /**
-     * @brief Creates a new greeter
-     * @param name the name to greet
-     */
-    Greeter(std::string name);
+    /**  Language codes to be used with the Greeter class */
+    enum class LanguageCode { EN, DE, ES, FR };
 
     /**
-     * @brief Creates a localized string containing the greeting
-     * @param lang the language to greet in
-     * @return a string containing the greeting
+     * @brief A class for saying hello in multiple languages
      */
-    std::string greet(LanguageCode lang = LanguageCode::EN) const;
-  };
+    class Greeter {
+        std::string name;
 
-  Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
+      public:
+        /**
+         * @brief Creates a new greeter
+         * @param name the name to greet
+         */
+        Greeter(std::string name);
 
-  std::string Greeter::greet(LanguageCode lang) const {
-    switch (lang) {
-      default:
-      case LanguageCode::EN:
-        return fmt::format("Hello, {}!", name);
-      case LanguageCode::DE:
-        return fmt::format("Hallo {}!", name);
-      case LanguageCode::ES:
-        return fmt::format("¡Hola {}!", name);
-      case LanguageCode::FR:
-        return fmt::format("Bonjour {}!", name);
+        /**
+         * @brief Creates a localized string containing the greeting
+         * @param lang the language to greet in
+         * @return a string containing the greeting
+         */
+        std::string greet(LanguageCode lang = LanguageCode::EN) const;
+    };
+
+    Greeter::Greeter(std::string _name) : name(std::move(_name)) {}
+
+    std::string Greeter::greet(LanguageCode lang) const {
+        switch (lang) {
+            default:
+            case LanguageCode::EN:
+                return fmt::format("Hello, {}!", name);
+            case LanguageCode::DE:
+                return fmt::format("Hallo {}!", name);
+            case LanguageCode::ES:
+                return fmt::format("¡Hola {}!", name);
+            case LanguageCode::FR:
+                return fmt::format("Bonjour {}!", name);
+        }
     }
-  }
 
 }  // namespace mpqsort
--- test/source/mpqsort.cpp
+++ test/source/mpqsort.cpp
@@ -1,21 +1,21 @@
-#include <catch2/catch.hpp>
 #include <mpqsort/mpqsort.h>
 #include <mpqsort/version.h>
 
+#include <catch2/catch.hpp>
 #include <string>
 
 TEST_CASE("Greeter") {
-  using namespace mpqsort;
+    using namespace mpqsort;
 
-  Greeter greeter("Tests");
+    Greeter greeter("Tests");
 
-  CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
-  CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
-  CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
-  CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
+    CHECK(greeter.greet(LanguageCode::EN) == "Hello, Tests!");
+    CHECK(greeter.greet(LanguageCode::DE) == "Hallo Tests!");
+    CHECK(greeter.greet(LanguageCode::ES) == "¡Hola Tests!");
+    CHECK(greeter.greet(LanguageCode::FR) == "Bonjour Tests!");
 }
 
 TEST_CASE("Greeter version") {
-  static_assert(std::string_view(MPQSORT_VERSION) == std::string_view("1.0"));
-  CHECK(std::string(MPQSORT_VERSION) == std::string("1.0"));
+    static_assert(std::string_view(MPQSORT_VERSION) == std::string_view("1.0"));
+    CHECK(std::string(MPQSORT_VERSION) == std::string("1.0"));
 }
