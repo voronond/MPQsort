3bea07445d2b64aa6a77e1235d3ddaa284f08671
Ondrej Voronecky <ondrej.voronecky@icewarp.com>
2022-08-18 23:15:36 +0200

    Provided default implementation for sorts

--- source/mpqsort.cpp
+++ source/mpqsort.cpp
@@ -2,62 +2,92 @@
 #include <omp.h>
 
 #include <exception>
+#include <limits>
 #include <vector>
+
+using namespace mpqsort;
 
 // TODO remove after all methods implemented
 #define UNUSED(x) (void)(x)
 
-using namespace mpqsort;
-
-template <typename RandomIt> void sort(RandomIt first, RandomIt last) {
-    UNUSED(first);
-    UNUSED(last);
-    throw std::logic_error("Not implemented");
+// Helper functions
+// Test if received policy is known
+template <typename T> inline constexpr void _is_execution_policy_assert() {
+    static_assert(execution::is_execution_policy_v<T>,
+                  "Provided ExecutionPolicy is not valid. Use predefined policies from namespace "
+                  "mpqsort::execution.");
 }
 
+// Call sort based on policy type
+template <typename ExecutionPolicy, typename... T> constexpr void _call_sort(T... args) {
+    _is_execution_policy_assert<ExecutionPolicy>();
+
+    if constexpr (std::is_same_v<ExecutionPolicy, decltype(execution::seq)>) {
+        // Call with one pivot
+        seq_multiway_qsort(1, std::forward<T...>(args...));
+    } else if constexpr (std::is_same_v<ExecutionPolicy, decltype(execution::seq_two_way)>) {
+        // Call with two pivots
+        seq_multiway_qsort(2, std::forward<T...>(args...));
+    } else if constexpr (std::is_same_v<ExecutionPolicy, decltype(execution::seq_multi_way)>) {
+        // Let algorithm decide how many pivots to use
+        seq_multiway_qsort(std::numeric_limits<int>::max, std::forward<T...>(args...));
+    } else if constexpr (std::is_same_v<ExecutionPolicy, decltype(execution::par)>) {
+        // Call with one pivot
+        par_multi_way_qsort(1, std::forward<T...>(args...));
+    } else if constexpr (std::is_same_v<ExecutionPolicy, decltype(execution::par_two_way)>) {
+        // Call with two pivots
+        par_multi_way_qsort(2, std::forward<T...>(args...));
+    } else if constexpr (std::is_same_v<ExecutionPolicy, decltype(execution::par_multi_way)>) {
+        // Let algorithm decide how many pivots to use
+        par_multi_way_qsort(std::numeric_limits<int>::max, std::forward<T...>(args...));
+    }
+}
+
+// Implementation of sort functions
+
+template <typename RandomIt> void sort(RandomIt first, RandomIt last) {
+    seq_multiway_qsort(1, first, last);
+}
+
+// TODO use enable_if if possible
 template <typename ExecutionPolicy, typename RandomIt>
 void sort(ExecutionPolicy&& policy, RandomIt first, RandomIt last) {
-    UNUSED(policy);
-    UNUSED(first);
-    UNUSED(last);
-    throw std::logic_error("Not implemented");
+    _call_sort(std::forward(policy), first, last);
 }
 
 template <typename ExecutionPolicy, typename Cores, typename RandomIt>
 void sort(ExecutionPolicy&& policy, Cores cores, RandomIt first, RandomIt last) {
-    UNUSED(policy);
-    UNUSED(first);
-    UNUSED(last);
-    UNUSED(cores);
-    throw std::logic_error("Not implemented");
+    _call_sort(std::forward(policy), cores, first, last);
 }
 
 template <typename RandomIt, typename Compare>
 void sort(RandomIt first, RandomIt last, Compare comp) {
-    UNUSED(first);
-    UNUSED(last);
-    UNUSED(comp);
-    throw std::logic_error("Not implemented");
+    seq_multiway_qsort(1, first, last, comp);
 }
 
 template <typename ExecutionPolicy, typename RandomIt, typename Compare>
 void sort(ExecutionPolicy&& policy, RandomIt first, RandomIt last, Compare comp) {
-    UNUSED(policy);
-    UNUSED(first);
-    UNUSED(last);
-    UNUSED(comp);
-    throw std::logic_error("Not implemented");
+    _call_sort(std::forward(policy), first, last, comp);
 }
 
 template <typename ExecutionPolicy, typename Cores, typename RandomIt, typename Compare>
 void sort(ExecutionPolicy&& policy, Cores cores, RandomIt first, RandomIt last, Compare comp) {
-    UNUSED(policy);
-    UNUSED(cores);
-    UNUSED(first);
-    UNUSED(last);
-    UNUSED(comp);
-    throw std::logic_error("Not implemented");
+    _call_sort(std::forward(policy), cores, first, last, comp);
 }
 
-// seq_multiway_qsort
-// par_multiway_qsort
+template <typename NumPivot, typename RandomIt, typename Compare = std::less<RandomIt>>
+void seq_multiway_qsort(NumPivot pivot_num, RandomIt first, RandomIt last, Compare comp) {
+    // TODO implement
+    UNUSED(pivot_num);
+    std::sort(first, last, comp);
+}
+
+template <typename NumPivot, typename Cores, typename RandomIt,
+          typename Compare = std::less<RandomIt>>
+void par_multiway_qsort(NumPivot pivot_num, Cores cores, RandomIt first, RandomIt last,
+                        Compare comp) {
+    // TODO implement
+    UNUSED(pivot_num);
+    UNUSED(cores);
+    std::sort(first, last, comp);
+}
--- test/source/mpqsort.cpp
+++ test/source/mpqsort.cpp
@@ -11,7 +11,21 @@ using namespace mpqsort;
 
 struct _TAG {
     const std::string IS_EXECUTION_POLICY = "[is_execution_policy_v]";
+    const std::string SEQ = "[seq_sort]";
+    const std::string SEQ_TWO_WAY = "[seq_two_way_sort]";
+    const std::string SEQ_MULTIWAY = "[seq_multiway_sort]";
+    const std::string SEQ_ALL = "[@seq_all]";
+    const std::string PAR = "[par_sort]";
+    const std::string PAR_TWO_WAY = "[par_two_way_sort]";
+    const std::string PAR_MULTIWAY = "[par_multiway_sort]";
+    const std::string PAR_ALL = "[@par_all]";
 } TAG;
+
+// Combine tags and create alias for catch2 (supports only C strings)
+CATCH_REGISTER_TAG_ALIAS(TAG.SEQ_ALL.c_str(),
+                         (TAG.SEQ + TAG.SEQ_TWO_WAY + TAG.SEQ_MULTIWAY).c_str());
+CATCH_REGISTER_TAG_ALIAS(TAG.PAR_ALL.c_str(),
+                         (TAG.PAR + TAG.PAR_TWO_WAY + TAG.PAR_MULTIWAY).c_str());
 
 // Declare allowed execution policies
 using ALLOWED_EXECUTION_POLICIES
@@ -26,13 +40,15 @@ using DISALLOWED_EXECUTION_POLICIES
 // TEST CASES
 
 // Type trait should return true for all allowed policies
-TEMPLATE_LIST_TEST_CASE("Execution policy type trait allowed policies", TAG.IS_EXECUTION_POLICY, ALLOWED_EXECUTION_POLICIES) {
+TEMPLATE_LIST_TEST_CASE("Execution policy type trait allowed policies", TAG.IS_EXECUTION_POLICY,
+                        ALLOWED_EXECUTION_POLICIES) {
     REQUIRE(std::is_swappable_v<int>);
     CHECK(execution::is_execution_policy_v<TestType>);
 }
 
 // Type trait should return false for all disallowed policies
-TEMPLATE_LIST_TEST_CASE("Execution policy type trait disallowed policies", TAG.IS_EXECUTION_POLICY, DISALLOWED_EXECUTION_POLICIES) {
+TEMPLATE_LIST_TEST_CASE("Execution policy type trait disallowed policies", TAG.IS_EXECUTION_POLICY,
+                        DISALLOWED_EXECUTION_POLICIES) {
     CHECK_FALSE(execution::is_execution_policy_v<TestType>);
 }
 
