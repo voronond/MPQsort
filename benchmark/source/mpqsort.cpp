719f29d5642413b9ecfde78ee63bf8ccdd219425
Ondrej Voronecky <ondrej.voronecky@icewarp.com>
2022-08-26 12:47:02 +0200

    Add parallel sorts in benchmarks and libs

--- benchmark/source/mpqsort.cpp
+++ benchmark/source/mpqsort.cpp
@@ -2,19 +2,57 @@
 #include <mpqsort/mpqsort.h>
 #include <mpqsort/version.h>
 
+#include <algorithm>
 #include <string>
+#include <vector>
+#include <parallel/algorithm>
+#include <tbb/tbb.h>
+#include <cilk/cilk.h>
 
-static void BM_StringCreation(benchmark::State& state) {
-    for (auto _ : state) std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+// TODO just for testing purposes
+std::vector<int> vec;
 
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-    using namespace mpqsort;
+static void BM_std_sort(benchmark::State& state) {
 
     for (auto _ : state) {
+        std::sort(vec.begin(), vec.end());
     }
 }
-BENCHMARK(BM_StringCopy);
+// Register the function as a benchmark
+BENCHMARK(BM_std_sort);
+
+static void BM_gnu_qs_sort(benchmark::State& state) {
+    for (auto _ : state) {
+        // In-place parallel qsort
+        __gnu_parallel::sort(vec.begin(), vec.end(), __gnu_parallel::quicksort_tag());
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_gnu_qs_sort);
+
+static void BM_gnu_bqs_sort(benchmark::State& state) {
+    for (auto _ : state) {
+        // In-place balanced parallel qsort
+        __gnu_parallel::sort(vec.begin(), vec.end(), __gnu_parallel::balanced_quicksort_tag());
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_gnu_bqs_sort);
+
+static void BM_gnu_mwms_sort(benchmark::State& state) {
+    for (auto _ : state) {
+        // Out-of-place parallel mergesort
+        __gnu_parallel::sort(vec.begin(), vec.end(), __gnu_parallel::multiway_mergesort_tag());
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_gnu_mwms_sort);
+
+static void BM_tbb_sort(benchmark::State& state) {
+    for (auto _ : state) {
+        // Out-of-place parallel mergesort
+        tbb::parallel_sort(vec.begin(), vec.end());
+    }
+}
+// Register the function as a benchmark
+BENCHMARK(BM_tbb_sort);
